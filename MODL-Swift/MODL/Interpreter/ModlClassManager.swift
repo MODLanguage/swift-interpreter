d0c7124a21a779d1c62b43ca053f76db6539406d
Nick Jones <nick.jones@touchsoft.co.uk>
2019-05-31 18:29:40 +0100

    default key name

--- MODL-Swift/MODL/Interpreter/ModlClassManager.swift
+++ MODL-Swift/MODL/Interpreter/ModlClassManager.swift
@@ -133,14 +133,16 @@ class ModlClassManager {
                     }) ?? assignList.sorted(by: { (first, second) -> Bool in
                         return first.values.count > second.values.count
                     }).first {
-                        for (index, matchingKey) in matching.values.enumerated() {
+                        for matchingKey in matching.values{
                             if let keyStr = (matchingKey as? ModlPrimitive)?.asString() {
                                 let value = pValue
                                 replacement.addValue(key: keyStr, value: value)
                             }
                         }
                     }
-                    replacement.addValue(key: "value", value: pValue)
+                    if replacement.values.count == 0 {
+                        replacement.addValue(key: "value", value: pValue)
+                    }
                 }
                 let extraDetails = constructAdditionalItems(classObj.name)
                 for detail in extraDetails {
