46f4cfe29495fbc98bf8361fadb22c9862d636f8
Nicholas Jones <nick.jones@touchsoft.co.uk>
2019-06-14 15:20:18 +0100

    version greater than 0

--- Source/Generator/ModlObjectCreator.swift
+++ Source/Generator/ModlObjectCreator.swift
@@ -224,7 +224,7 @@ internal class ModlObjectCreator {
         switch uwReserved {
         case .version, .versionSH:
             //Could raise an error here for non-matching version.... although json test implies it just continues
-            if let mPrim = value as? ModlPrimitive, let decVersion = mPrim.asNumber(), key == key?.uppercased() {
+            if let mPrim = value as? ModlPrimitive, let decVersion = mPrim.asNumber(), key == key?.uppercased(), decVersion > 0 {
                 if Double(truncating: decVersion as NSNumber) != ModlListener.ModlVersion {
                     //Could raise an error here for non-matching version.... although json test implies it just continues
                     warnings.append(InterpreterError.mismatchedVersion)
